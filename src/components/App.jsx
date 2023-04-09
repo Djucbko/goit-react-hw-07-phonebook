fadd428252e445f18443dea5739ae9bf6606b551
Djucbko <109214332+Djucbko@users.noreply.github.com>
2023-04-09 11:19:10 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import Home from './Home/Home';
+import ContactList from './ContactList/ContactList';
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contacts" element={<ContactList />}></Route>
+      </Routes>
     </div>
   );
 };
